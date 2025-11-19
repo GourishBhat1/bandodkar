2c14e416be73b16fe779f1e988e259a6f809b974
GourishBhat <gyronotechgoa@gmail.com>
2025-11-19 10:39:53 +0530

    jquery added

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -12,6 +12,7 @@
 <!-- ===============================================-->
 <!--    JavaScripts-->
 <!-- ===============================================-->
+ <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="vendors/popper/popper.min.js"></script>
 <script src="vendors/bootstrap/bootstrap.min.js"></script>
 <script src="vendors/anchorjs/anchor.min.js"></script>
